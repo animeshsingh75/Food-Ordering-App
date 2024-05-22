dde670e75ec2855d0b5a70bb3c07b3236f509759
Animesh <animeshsingh75@gmail.com>
2024-05-22 12:54:46 -0700

    Added filter functionality

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+const Footer = () => {
+  const year = new Date().getFullYear();
+  return (
+    <div className="footer">
+      Created By
+      <i className="fa-solid fa-heart"></i>
+      <a href="https://www.linkedin.com/in/animeshsingh75/" target="_blank">
+        Animesh Singh
+      </a>
+      <i className="fa-solid fa-copyright"></i>
+      {year}
+      <strong>
+        Food<span>Fire</span>
+      </strong>
+    </div>
+  );
+};
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import { APP_LOGO } from "../utils/constants";
+const Title = () => (
+  <a href="/">
+    <img className="logo" src={APP_LOGO} alt="Food Logo" />
+  </a>
+);
+const Header = () => {
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>
+            <i className="fa-solid fa-cart-shopping"></i>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
